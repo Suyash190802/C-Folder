908ca375d786235c3737bb0ad2814fffb5cedbe5
suyash choudhary <suyashchoudhary76@gmail.com>
2026-08-27 17:29:27 +0530

    pointer to object

--- day7C++.cpp
+++ day7C++.cpp
@@ -1,4 +1,208 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+class Student
+{
+private:
+    std::string name;
+    int marks;
+    int attendance;
+public:
+    Student(std::string n, int m, int a)
+        : name(n), marks(m), attendance(a)
+    {
+    }
+    void display()
+    {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Marks: " << marks << std::endl;
+        std::cout << "Attendance: " << attendance << std::endl;
+    }
+    bool isEligible()
+    {
+        return attendance >= 75;
+    }
+    static std::string Student::* getNamePointer()
+    {
+        return &Student::name;
+    }
+    static int Student::* getMarksPointer()
+    {
+        return &Student::marks;
+    }
+    static int Student::* getAttendancePointer()
+    {
+        return &Student::attendance;
+    }
+    static void displayMember(Student& s, int Student::* member)
+    {
+        std::cout << s.*member << std::endl;
+    }
+     static void displayName(Student& s, std::string Student::* member)
+    {
+        std::cout << s.*member << std::endl;
+    }
+};
+int main()
+{
+    Student s1("Suyash", 85, 95);
+    Student s2("Diksha", 31, 33);
+    Student s3("MSD", 100, 100);
+    int Student::* members[2];
+    members[0] = Student::getMarksPointer();
+    members[1] = Student::getAttendancePointer();
+    std::string Student::* member[1];
+    member[0] = Student::getNamePointer();
+    Student* students[3] = {&s1, &s2, &s3};
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "\nStudent " << i + 1 << ":" << std::endl;
+        std::cout << "Name: ";
+        Student::displayName(*students[i], member[0]);
+        std::cout << "Marks: ";
+        Student::displayMember(*students[i], members[0]);
+        std::cout << "Attendance: ";
+        Student::displayMember(*students[i], members[1]);
+        std::cout << "Eligibility: ";
+        if (students[i]->isEligible())
+            std::cout << "Eligible" << std::endl;
+        else
+            std::cout << "Not Eligible" << std::endl;
+    }
+    return 0;
+}
+
+
+
+/* #include<iostream>
+#include<string>
+class Library
+{
+  public:
+   class Book
+   {
+     private:
+       std::string title;
+       std::string author;
+       public:
+        Book(std::string t, std::string a): title(t), author(a)
+        {
+            std::cout << "Book constructed: "<< title << std::endl;
+        }
+        ~Book()
+        {
+            std::cout << "Book destroyed: "<< title << std::endl;
+        }
+        void display()
+        {
+            std::cout << "Title: " << title << std::endl;
+            std::cout << "Author: " << author << std::endl;
+        }
+   };
+};
+int main()
+{
+    Library::Book books[3] =
+    {
+        Library::Book("C++ Primer","Stanley Lippman"),
+        Library::Book("Clean Code","Robert Martin"),
+        Library::Book("Effective C++","Scott Meyers")
+    };
+    std::cout << "Library Books:"<<std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout<<"Book"<<i+1<<":" <<std::endl;
+        books[i].display();
+    }
+  return 0;
+} */
+/* #include <iostream>
+#include<string>
+class Shape
+{
+public:
+    virtual double area() = 0;
+    virtual double perimeter() = 0;
+    virtual ~Shape()
+    {
+    }
+};
+class Circle : public Shape
+{
+private:
+    double radius;
+public:
+    Circle(double r)
+    {
+        radius = r;
+    }
+    double area() override
+    {
+        return 3.14 * radius * radius;
+    }
+    double perimeter() override
+    {
+        return 2 * 3.14 * radius;
+    }
+};
+class Rectangle : public Shape
+{
+private:
+    double length, width;
+public:
+    Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+    double area() override
+    {
+        return length * width;
+    }
+    double perimeter() override
+    {
+        return 2 * (length + width);
+    }
+};
+class Triangle : public Shape
+{
+private:
+    double a, b, c;
+
+public:
+    Triangle(double x, double y, double z)
+    {
+        a = x;
+        b = y;
+        c = z;
+    }
+    double area() override
+    {
+        double s = (a + b + c) / 2;
+        return s * (s - a) * (s - b) * (s - c);
+    }
+    double perimeter() override
+    {
+        return a + b + c;
+    }
+};
+int main()
+{
+    Shape* shapes[3];
+    shapes[0] = new  Circle(5);
+    shapes[1] = new Rectangle (10, 5);
+    shapes[2] = new Triangle (3, 4, 5);
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout<< "Area: " << shapes[i]->area() <<std::endl;
+        std::cout<< "Perimeter: " << shapes[i]->perimeter() <<std::endl;
+        //cout << endl;
+    }
+    delete shapes[0];
+    delete shapes[1];
+    delete shapes[2];
+    return 0;
+} */
+/* #include<iostream>
 #include<string>
 class Employee
 {
@@ -34,8 +238,7 @@ class Developer : protected Employee
 {
 public:
     std::string programmingLanguage;
-    Developer(std::string n, double s, int id,std:: string lang)
-        : Employee(n, s, id), programmingLanguage(lang) {}
+    Developer(std::string n, double s, int id,std:: string lang) : Employee(n, s, id), programmingLanguage(lang) {}
     void displayDeveloperInfo()
     {
         std::cout << "\n--Developer--" <<std::endl;
@@ -48,8 +251,7 @@ class Intern : private Employee
 {
 public:
     std::string university;
-    Intern(std::string n, double s, int id, std::string u)
-        : Employee(n, s, id), university(u) {}
+    Intern(std::string n, double s, int id, std::string u) : Employee(n, s, id), university(u) {}
     void displayInternInfo()
     {
         std::cout<< "\n--Intern--" <<std::endl;
@@ -66,8 +268,7 @@ class TeamLead : virtual public Employee
 class SeniorDeveloper : virtual public Employee
 {
 public:
-    SeniorDeveloper(std::string n, double s, int id)
-        : Employee(n, s, id) {}
+    SeniorDeveloper(std::string n, double s, int id) : Employee(n, s, id) {}
 };
 class TechLead : public TeamLead, public SeniorDeveloper
 {
@@ -86,10 +287,10 @@ public:
 };
   int main()
 {
-    Manager m("Suyash", 75000, 101, 10);
-    Developer d("Ms Dhoni", 65000, 102, "C++");
-    Intern i("Virat", 25000, 103, "Delhi University");
-    TechLead t("Rohit", 120000, 104);
+    Manager m("Suyash",75000,101,10);
+    Developer d("Ms Dhoni",65000,102,"C++");
+    Intern i("Virat",25000,103,"Delhi University");
+    TechLead t("Rohit",120000,104);
     m.displayManagerInfo();
     std::cout << "\nManager basic information:" <<std::endl;
     m.displayBasicInfo();
@@ -100,9 +301,7 @@ public:
     m.displayBasicInfo();
     std::cout<< "\nTechLead name directly: "<< t.name <<std::endl;
     return 0;
-}
-
-
+} */
 /*  #include <iostream>
 #include <string>
 class Person
